b77c107d0c24c7c1c6217cb3ec55ec572e24f6a0
KnotPP <pattaraponknot@gmail.com>
2022-04-15 18:26:31 +0700

    Link to firebase

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBus5Xs8QPbMbXWvQhmkx9rXWu9jwoQIkc",
+  authDomain: "comp-eng-ess-group27.firebaseapp.com",
+  projectId: "comp-eng-ess-group27",
+  storageBucket: "comp-eng-ess-group27.appspot.com",
+  messagingSenderId: "697425701371",
+  appId: "1:697425701371:web:f69be2e4281cf07221035e",
+  measurementId: "G-S4GDG9NSP0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  getFirestore,
+  updateDoc,
+} from 'https://www.gstatic.com/firebasejs/9.6.11/firebase-firestore.js';
+
+const db = getFirestore();
+const users_ref = collection(db, 'users');
+
+
 function myFunction() {
     var x = document.getElementById("password");
     if (x.type === "password") {
